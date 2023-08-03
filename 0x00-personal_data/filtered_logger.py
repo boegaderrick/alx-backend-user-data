2df1f80e3d03fb58dd7ebe3b71414bc063f351e0
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-03 14:40:24 +0300

    RedactingFormatter class definition

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Logger module"""
 from typing import List
+import logging
 import re
 
 
@@ -11,3 +12,25 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{i}.*?{separator}',
                          f'{i}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialization method"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.FIELDS = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+            This method takes a log record, calls a function that redacts the
+            values of keys specified in 'FIELDS' attribute then passes the new
+            redacted log to the parent class' format method. The parents method's
+            return value is then returned to the original caller.
+        """
+        record.msg = filter_datum(self.FIELDS, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)

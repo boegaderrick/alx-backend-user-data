1a7dbe5b648718c7f5a9694c3c34dc28ff134ca4
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-03 16:46:26 +0300

    get_logger function definition

--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -10,4 +10,5 @@ PII_FIELDS = __import__('filtered_logger').PII_FIELDS
 
 print(get_logger.__annotations__.get('return'))
 print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+print(get_logger().handlers)
 

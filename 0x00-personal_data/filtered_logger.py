227bd25f0d74c5d9a60fd920641d66772e5d6530
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-03 13:54:43 +0300

    repo README file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@
 import re
 
 
-def filter_datum(fields, redaction, msg, sep):
+def filter_datum(fields: List[str], redaction: str, msg: str, sep: str) -> str:
     """Redacts 'msg' values of all keys specified in 'fields'"""
     for i in fields:
         msg = re.sub(f'{i}.*?{sep}', f'{i}={redaction}{sep}', msg)

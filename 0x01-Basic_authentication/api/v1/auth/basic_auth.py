36005342d97cbba651e395576964b2df61df67db
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-08 17:45:08 +0300

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """This module contains a class that inherits from Auth"""
 from api.v1.auth.auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
     """Class definition"""
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """
             This method extracts the encoded authentication credentials
             from the header value and returns it.
@@ -19,3 +21,16 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.lstrip('Basic ')
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """This method decodes the base64 header"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            ret = base64.b64decode(base64_authorization_header)
+            return ret.decode()
+        except binascii.Error:
+            return None

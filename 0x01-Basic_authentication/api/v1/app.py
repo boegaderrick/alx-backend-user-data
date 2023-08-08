4ed735c2ea47076c5e6571aa7a70c0cf74e46904
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-08 17:04:49 +0300

    authentication type logic

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,9 +13,14 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if os.getenv('AUTH_TYPE'):
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+auth_type = os.getenv('AUTH_TYPE'):
+if auth_type:
+    if auth_type == 'basic_auth':
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
+    else:
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 
 @app.before_request

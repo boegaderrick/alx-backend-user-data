14abaa6838d9f394fb8edb843a847c828e0b6e8e
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-08 17:29:46 +0300

    base64 value extraction logic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,3 +5,16 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Class definition"""
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+            This method extracts the encoded authentication credentials
+            from the header value and returns it.
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+
+        return authorization_header.lstrip('Basic ')

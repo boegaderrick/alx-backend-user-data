04bbf728b59f9e24855e2881c80e137649af308a
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-08 14:36:07 +0300

    Auth.require_auth method definition

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,8 +7,10 @@ from typing import List, TypeVar
 class Auth:
     """Class definition"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require method"""
-        return False
+        """This method checks if 'path' is in 'excluded_paths'"""
+        if not path or not excluded_paths or len(excluded_paths) == 0:
+            return True
+        return not any(i.startswith(path.rstrip('/')) for i in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
         """Authorization method"""

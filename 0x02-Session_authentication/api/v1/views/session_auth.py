2730c0d66f7c139c00bcef2d909e55781425df5b
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-10 18:03:57 +0300

    login route implementation

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module contains the login route"""
 from api.v1.views import app_views
-from flask import jsonify, request, session
+from flask import jsonify, make_response, request
 from models.user import User
 from os import getenv
 from typing import TypeVar
@@ -12,24 +12,24 @@ def login():
     """Login function definition"""
     email: str = request.form.get('email')
     if not email or len(email) == 0:
-        return jsonify({'error': 'email missing'})
+        return make_response(jsonify({'error': 'email missing'}), 400)
     pwd: str = request.form.get('password')
     if not pwd or len(pwd) == 0:
-        return jsonify({'error': 'password missing'})
+        return make_response(jsonify({'error': 'password missing'}), 400)
 
     try:
         users: List[TypeVar['User']] = User.search({'email': email})
     except (AttributeError, Exception):
         users = []
     if len(users) == 0:
-        return jsonify({'error': 'no user found for this email'})
+        return make_response(jsonify({'error': 'no user found for this email'}), 404)
 
     for user in users:
         if user.is_valid_password(pwd):
             from api.v1.app import auth
             session_id = auth.create_session(user.id)
-            response = jsonify(user.to_json())
+            response = make_response(jsonify(user.to_json()), 200)
             response.set_cookie(getenv('SESSION_NAME'), session_id)
             return response
 
-    return jsonify({'error': 'wrong password'})
+    return make_response(jsonify({'error': 'wrong password'}), 401)

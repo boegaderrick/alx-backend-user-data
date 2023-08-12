301f51d0a95ab4638174d93de2bc7c28bd3c69fa
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-08-12 22:58:03 +0300

    SessionDBAuth and UserSession classes bugfix

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -18,16 +18,17 @@ class SessionDBAuth(SessionExpAuth):
             session_id=session_id
         )
         user_session.save()
-        return user_session.id
+        return user_session.session_id
 
     def user_id_for_session_id(self, session_id=None):
         """This method returns the user_id associated with a session"""
         if super().user_id_for_session_id(session_id) is None:
+            if len(self.user_id_by_session_id):
+                return None
+        sessions = UserSession.search({'session_id': session_id})
+        if not len(sessions):
             return None
-        session = UserSession.get(session_id)
-        if not session:
-            return None
-        return session.user_id
+        return sessions[0].user_id
 
     def destroy_session(self, request=None):
         """This method destroys a session"""
@@ -36,9 +37,9 @@ class SessionDBAuth(SessionExpAuth):
         if not session_id:
             return False
 
-        session = UserSession.get(session_id)
-        if not session:
+        sessions = UserSession.search({'session_id': session_id})
+        if not len(sessions):
             return False
 
-        session.remove()
+        sessions[0].remove()
         return True
